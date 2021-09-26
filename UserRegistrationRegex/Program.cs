c5edf50766be3d606d1773e0c4fb3318665ede8c
ka1peshc <kalpesh111997@gmail.com>
2021-09-26 18:37:48 +0530

    Perform Regular expression on first name

---
+++ UserRegistrationRegex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string pattern = @"^[A-Z]{1}[a-z]{2,20}$";
+            Regex rx = new Regex(pattern);
+            Console.WriteLine("Enter First name");
+            string firstname = Console.ReadLine();
+            Console.WriteLine(rx.Match(firstname));
+            
+        }
+    }
+}
