cacc02e118f72cf70a39efec1dd746e911b23986
ka1peshc <kalpesh111997@gmail.com>
2021-09-26 19:00:01 +0530

    Last name validation check

--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -8,12 +8,31 @@ namespace UserRegistrationRegex
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string pattern = @"^[A-Z]{1}[a-z]{2,20}$";
-            Regex rx = new Regex(pattern);
+            //Expression for name
+            string NamePattern = @"^[A-Z]{1}[a-z]{2,20}$";
+            Regex rx = new Regex(NamePattern);
             Console.WriteLine("Enter First name");
             string firstname = Console.ReadLine();
-            Console.WriteLine(rx.Match(firstname));
-            
+            if(rx.IsMatch(firstname) == true)
+            {
+                Console.WriteLine("{0} pass regular expression",firstname);
+            }
+            else
+            {
+                Console.WriteLine("{0} fail to pass regular expression",firstname);
+            }
+
+            Console.WriteLine("Enter last name");
+            string lastname = Console.ReadLine();
+            if(rx.IsMatch(lastname) == true)
+            {
+                Console.WriteLine("{0} pass regular expression", lastname);
+            }
+            else
+            {
+                Console.WriteLine("{0} pass regular expression", lastname);
+            }
+
         }
     }
 }
