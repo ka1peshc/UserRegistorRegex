8430bcc2ce83445d96061d5cf457a945114e49fb
ka1peshc <kalpesh111997@gmail.com>
2021-09-27 08:16:41 +0530

    Added Email And  Phone NUmber regular expression

--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -8,30 +8,30 @@ namespace UserRegistrationRegex
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //Expression for name
-            string NamePattern = @"^[A-Z]{1}[a-z]{2,20}$";
-            Regex rx = new Regex(NamePattern);
-            Console.WriteLine("Enter First name");
-            string firstname = Console.ReadLine();
-            if(rx.IsMatch(firstname) == true)
+            RegularExpression re = new RegularExpression();
+            Console.WriteLine("Enter option number");
+            Console.WriteLine("1. First name\n2. Second Name\n3. Email\n4. Phone number");
+            int opt = int.Parse(Console.ReadLine());
+            switch (opt)
             {
-                Console.WriteLine("{0} pass regular expression",firstname);
-            }
-            else
-            {
-                Console.WriteLine("{0} fail to pass regular expression",firstname);
-            }
+                case 1:
+                    re.FirstName();
+                    break;
+                case 2:
+                    re.LastName();
+                    break;
+                case 3:
+                    re.Email();
+                    break;
+                case 4:
+                    re.PhoneNumber();
+                    break;
+                default:
+                    break;
 
-            Console.WriteLine("Enter last name");
-            string lastname = Console.ReadLine();
-            if(rx.IsMatch(lastname) == true)
-            {
-                Console.WriteLine("{0} pass regular expression", lastname);
             }
-            else
-            {
-                Console.WriteLine("{0} pass regular expression", lastname);
-            }
+            
+            
 
         }
     }
---
+++ UserRegistrationRegex/RegularExpression.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace UserRegistrationRegex
+{
+    class RegularExpression
+    {
+        string NamePattern = @"^[A-Z]{1}[a-z]{2,20}$";
+        
+        internal void FirstName()
+        {
+            Regex rx = new Regex(NamePattern);
+            Console.WriteLine("Enter First name");
+            string firstname = Console.ReadLine();
+            if (rx.IsMatch(firstname) == true)
+            {
+                Console.WriteLine("{0} pass regular expression", firstname);
+            }
+            else
+            {
+                Console.WriteLine("{0} fail to pass regular expression", firstname);
+            }
+        }
+        internal void LastName()
+        {
+            Regex rx = new Regex(NamePattern);
+            Console.WriteLine("Enter last name");
+            string lastname = Console.ReadLine();
+            if (rx.IsMatch(lastname) == true)
+            {
+                Console.WriteLine("{0} pass regular expression", lastname);
+            }
+            else
+            {
+                Console.WriteLine("{0} pass regular expression", lastname);
+            }
+        }
+        internal void Email()
+        {
+            string emailRE = @"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+            //string emailRE = @"^[A-Za-z0-9]+[@][a-z]+[\.][a-z](2,3)$";
+            Regex rx = new Regex(emailRE);
+            Console.WriteLine("Enter email address");
+            string email = Console.ReadLine();
+            if (rx.IsMatch(email))
+            {
+                Console.WriteLine("{0} valid email",email);
+            }
+            else
+            {
+                Console.WriteLine("{0} Invalid email", email);
+            }
+        }
+
+        internal void PhoneNumber()
+        {
+            string phoneRE = @"^[0-9]{2}\s[0-9]{10}$";
+            Regex rx = new Regex(phoneRE);
+            Console.WriteLine("Enter phone number with contry code");
+            string phone = Console.ReadLine();
+            if (rx.IsMatch(phone))
+            {
+                Console.WriteLine("Valid phone number");
+            }
+            else
+            {
+                Console.WriteLine("Invalid phone number");
+            }
+        }
+    }
+}
